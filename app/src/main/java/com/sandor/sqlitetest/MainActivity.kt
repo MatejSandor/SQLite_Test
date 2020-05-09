453c9c759b0387e9cfc36fb4f93b9a835c19740a
Matej Šándor <matejko.sandor@gmail.com>
2020-05-09 10:38:44 +0200

    code reformat and replaced query with it to improve efficiency

--- app/src/main/java/com/sandor/sqlitetest/MainActivity.kt
+++ app/src/main/java/com/sandor/sqlitetest/MainActivity.kt
@@ -19,9 +19,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val database = baseContext.openOrCreateDatabase("sqlite-test-1.db", Context.MODE_PRIVATE, null)
+        val database =
+            baseContext.openOrCreateDatabase("sqlite-test-1.db", Context.MODE_PRIVATE, null)
         database.execSQL("DROP TABLE IF EXISTS contacts")
-        var sql = "CREATE TABLE IF NOT EXISTS contacts(_id INTEGER PRIMARY KEY NOT NULL, name TEXT, phone INTEGER, email TEXT)"
+        var sql =
+            "CREATE TABLE IF NOT EXISTS contacts(_id INTEGER PRIMARY KEY NOT NULL, name TEXT, phone INTEGER, email TEXT)"
         Log.d(TAG, "onCreate: sql is $sql")
         database.execSQL(sql)
 
@@ -36,13 +38,13 @@ class MainActivity : AppCompatActivity() {
             put("email", "fred@nurk.com")
         }
 
-        val generatedId = database.insert("contacts",  null, values)
+        val generatedId = database.insert("contacts", null, values)
         Log.d(TAG, "onCreate: record added with id $generatedId")
 
         val query = database.rawQuery("SELECT * FROM contacts", null)
         query.use {
-            while(it.moveToNext()) {
-                with(query) {
+            while (it.moveToNext()) {
+                with(it) {
                     val id = getLong(0)
                     val name = getString(1)
                     val phone = getInt(2)
@@ -58,7 +60,7 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
